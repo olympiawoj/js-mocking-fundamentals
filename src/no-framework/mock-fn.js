78761c527528dc8d657860730c74a31f16482a44
olympiawoj <owojcik7@gmail.com>
2020-08-10 09:13:38 -0400

    3. Ensure Functions are Called Correctl with JavaScript Mocks

--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -2,14 +2,13 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  mockFn.mockImplementation = newImpl => (impl = newImpl)
-  return mockFn
+function fn(impl){
+    const mockFn = (...args)=>{
+        mockFn.mock.calls.push(args)
+        return impl(...args)
+    }
+    mockFn.mock = {calls: []}
+    return mockFn
 }
 
 const originalGetWinner = utils.getWinner
@@ -18,9 +17,10 @@ utils.getWinner = fn((p1, p2) => p1)
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler']
+ ['Kent C. Dodds', 'Ken Wheeler'],
+ ['Kent C. Dodds', 'Ken Wheeler']
 ])
+
 
 // cleanup
 utils.getWinner = originalGetWinner
